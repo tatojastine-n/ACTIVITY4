fa312bb5db26d2e984d8662b334ca845f41f655e
tatojastine-n <jajanicoletato@gmail.com>
2025-08-07 13:47:50 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeelingHotOrCold
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius;
+            bool isValidInput;
+
+            do
+            {
+                Console.Write("Enter temperature in Celsius (-100 to 100): ");
+                isValidInput = float.TryParse(Console.ReadLine(), out celsius) && celsius >= -100 && celsius <= 100;
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Invalid input. Enter a number between -100 and 100.");
+                }
+            } while (!isValidInput);
+            float fahrenheit = (celsius * 9f / 5f) + 32;
+            Console.WriteLine( $"Temperature in Fahrenheit: {fahrenheit.ToString("0.##")}"); 
+        }
+    }
+}
